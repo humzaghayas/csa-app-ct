1bfe9b34907cb4ecf91a03bfc023298cdd3134db
Muhammad Humza Ghayas <humza.ghayas@royalcyber.com>
2022-12-16 13:08:52 +0500

    changes

--- ct-tickets-api/functions/index.js
+++ ct-tickets-api/functions/index.js
@@ -1,26 +1,10 @@
-// The Cloud Functions for Firebase SDK to create Cloud Functions and set up triggers.
 const functions = require('firebase-functions');
-
-// The Firebase Admin SDK to access Firestore.
 const admin = require('firebase-admin');
 admin.initializeApp();
 
 const customObjectsService = require('./services/tickets')();
-const CONSTANTS = require('./config/constants');
+const {getTicketCategories,getTicketPriorityValues} = require('ct-tickets-helper-api');
 
-// Take the text parameter passed to this HTTP endpoint and insert it into 
-// Firestore under the path /messages/:documentId/original
-// exports.getTickets = functions.https.onRequest(async (req, res) => {
-//     // Grab the text parameter.
-//     const original = req.query.text;
-
-
-//     const results =await customObjectsService.getTickets(CONSTANTS.SUBSCTIONTION_CONTAINER,
-//         CONSTANTS.SUBSCTIONTION_KEY);
-
-//        // res.status(200).send(formattedDate);
-//     res.status(200).json({result: `Message with ID: ${JSON.stringify(results)} added.`});
-//   });
 
 const express = require('express');
 const cors = require('cors');
@@ -36,9 +20,9 @@ app.get('/', async(req, res) => {
         let p = !page? 1:Number.parseInt(page);
         let perP = !perPage ?10: Number.parseInt(perPage);
         const results =await customObjectsService.getTickets(p,perP);
-        res.status(200).json({result: `${JSON.stringify(results)}`});
+        res.status(200).json({result: results});
     }catch(err){
-        res.status(400).json({Error: `${JSON.stringify(err)}`});
+        res.status(400).json({Error: err});
     }
 } );
 
@@ -48,12 +32,58 @@ app.post('/create-ticket', async(req, res) =>{
     const result =await customObjectsService.createTicket(data);
 
     if(result.error){
-        res.status(400).json({result: JSON.stringify(result.errors)});    
+        res.status(400).json({result: result.errors});    
     }else{
-        res.status(200).json({result: JSON.stringify(result)});
+        res.status(200).json({result:result});
     }
 
 });
 
-// Expose Express API as a single Cloud Function:
+app.get('/look-up/:field', async(req, res) =>{
+
+    const {field} = req.params;
+
+    switch(field) {
+        case 'all' :
+            const ticketCategoriesAll = getTicketCategories();
+            const getTicketCategoriesOptsAll = Object.keys(ticketCategoriesAll).map((key) => ({
+                label: ticketCategoriesAll[key],
+                value: key,
+              }));
+
+            const ticketPriorityValAll =getTicketPriorityValues();
+            const getTicketPriorityOptionsAll = Object.keys(ticketPriorityValAll).map((key) => ({
+                label: ticketCategoriesAll[key],
+                value: key,
+            }));  
+
+              res.status(200).json({
+                categories:getTicketCategoriesOptsAll,
+                priorities:getTicketPriorityOptionsAll
+            });
+          break;
+        case 'category' :
+            const ticketCategories = getTicketCategories();
+            const getTicketCategoriesOpts = Object.keys(ticketCategories).map((key) => ({
+                label: ticketCategories[key],
+                value: key,
+              }));
+
+              res.status(200).json({categories:getTicketCategoriesOpts});
+          break;
+        case 'priority':
+            const ticketPriorityVal =getTicketPriorityValues();
+            const getTicketPriorityOptions = Object.keys(ticketPriorityVal).map((key) => ({
+                label: ticketPriorityVal[key],
+                value: key,
+              }));
+
+              res.status(200).json({priorities:getTicketPriorityOptions});
+          break;
+        default:{
+            res.status(400).json({error:true,message :"Invalid lookup field!"});
+        }
+      }
+});
+
 exports.tickets = functions.https.onRequest(app);
