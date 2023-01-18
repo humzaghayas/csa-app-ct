9dc36a102a6c6d1d71f35d2dbff0eef6fb72b35b
Muhammad Humza Ghayas <humza.ghayas@royalcyber.com>
2023-01-18 12:11:11 +0500

    minor Change

--- ct-tickets-helper-api/lib/order-graphql-queries/index.js
+++
@@ -1,4 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.FETCH_ORDERS = void 0;
-exports.FETCH_ORDERS = "query FetchAllOrders($limit: Int!, $offset: Int!, $sort: [String!]){\n    orders(limit: $limit, offset: $offset, sort: $sort){\n      total\n      count\n      offset\n      results{\n        id\n        orderState\n        orderNumber\n        customer{\n            firstName\n            lastName\n        }\n        customerEmail\n        createdAt\n        lastModifiedAt\n        orderState\n        shippingInfo{\n            shippingMethodName\n            shippingMethodState\n        }    \n      }\n    }\n  } ";
