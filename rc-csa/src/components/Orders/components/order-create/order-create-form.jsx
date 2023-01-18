6f262ca9bd55ef0c469359fc613aac70688f4fb6
Lahari Ramurthi <lahari.r@royalcyber.com>
2023-01-18 13:41:07 +0530

    order UI

--- rc-csa/src/components/Orders/components/order-create/order-create-form.jsx
+++ rc-csa/src/components/Orders/components/order-create/order-create-form.jsx
@@ -12,20 +12,40 @@ import { EMPLOYEE_ROLES,CUSTOMER_GROUPS,TICKET_PRIORITY} from './constants';
 import CollapsiblePanel from '@commercetools-uikit/collapsible-panel';
 import Constraints from '@commercetools-uikit/constraints';
 import { PrimaryButton, SecondaryButton } from '@commercetools-uikit/buttons';
-// const getEmployeeRoleOptions = Object.keys(EMPLOYEE_ROLES).map((key) => ({
-//   label: EMPLOYEE_ROLES[key],
-//   value: EMPLOYEE_ROLES[key],
-// }));
+import DataTable from '@commercetools-uikit/data-table';
+//import iphone from './iphone.jpg'
 
-// const getCustomerGroupsOptions = Object.keys(CUSTOMER_GROUPS).map((key) => ({
-//   label: key,
-//   value: CUSTOMER_GROUPS[key],
-// }));
 const getTicketPriorityOptions = Object.keys(TICKET_PRIORITY).map((key) => ({
   label: key,
   value: TICKET_PRIORITY[key],
 }));
+const rows = [
+  {product: '', originalUnitPrice: '$350.00' ,UnitPrice:'$350.00',Qty:'3',LineItemState:'',subTotal:'$1150.00',Tax:"0",Total:'$1150.00'},
+  {product: '', originalUnitPrice: '$350.00' ,UnitPrice:'$350.00',Qty:'3',LineItemState:'',subTotal:'$1150.00',Tax:"0",Total:'$1150.00'},
+  {product: '', originalUnitPrice: '$350.00' ,UnitPrice:'$350.00',Qty:'3',LineItemState:'',subTotal:'$1150.00',Tax:"0",Total:'$1150.00'},
+];
 
+const columns = [
+  { key: 'product', label: 'product' 
+  ,renderItem: (row) => (
+    <div>
+      {/* <img src={iphone} alt="" /> */}
+      <Spacings.Stack scale='s'>
+     <div>iphone</div>
+     <div>SKU:iphone</div>
+     <div>Key:iphone</div>
+     </Spacings.Stack>
+  </div>
+  )
+  },
+  { key: 'originalUnitPrice', label: 'originalUnitPrice' },
+  { key: 'UnitPrice', label: 'UnitPrice' },
+  { key: 'Qty', label: 'Qty' },
+  { key: 'LineItemState', label: 'LineItemState' },
+  { key: 'subTotal', label: 'subTotal' },
+  { key: 'Tax', label: 'Tax' },
+  { key: 'Total', label: 'Total' },
+];
 
 const OrderCreateForm = (props) => {
   const intl = useIntl();
@@ -44,92 +64,22 @@ const OrderCreateForm = (props) => {
           header={
             <CollapsiblePanel.Header>
               {/* {formatMessage(messages.panelTitle)} */}
-              {'Order Details'}
+              {'Order Summary'}
             </CollapsiblePanel.Header>
           }
           scale="l">
             <Constraints.Horizontal >
              <Spacings.Stack scale="m">
-             <Spacings.Stack scale="s">
-        <TextField
-          name="Product Code"
-          title="Product Code"
-          value={formik.values.title}
-          errors={formik.errors.title}
-          touched={formik.touched.title}
-          onChange={formik.handleChange}
-          onBlur={formik.handleBlur}
-          horizontalConstraint={13}
-        />
-        </Spacings.Stack>
-        <Spacings.Stack scale="s">
-          <TextField
-          name="Product Name"
-          title="Product Name"
-          value={formik.values.firstName}
-          errors={formik.errors.firstName}
-          touched={formik.touched.firstName}
-          onChange={formik.handleChange}
-          onBlur={formik.handleBlur}
-          horizontalConstraint={13}
-        />
-     
-     </Spacings.Stack>
-     <Spacings.Stack scale="s">
-        <TextField
-          name="Item Price"
-          title="Item Price"
-          value={formik.values.middleName}
-          errors={formik.errors.middleName}
-          touched={formik.touched.middleName}
-          onChange={formik.handleChange}
-          onBlur={formik.handleBlur}
-          horizontalConstraint={13}
-        />
-        </Spacings.Stack>
-        <Spacings.Stack scale="s">
-        <TextField
-          name="allocated"
-          title="allocated"
-          value={formik.values.lastName}
-          errors={formik.errors.lastName}
-          touched={formik.touched.lastName}
-          onChange={formik.handleChange}
-          onBlur={formik.handleBlur}
-          horizontalConstraint={13}
-        />
-     </Spacings.Stack>
-     <Spacings.Stack scale="s">
+            
         
-        <TextField
-          name="returned"
-          title="returned"
-          isRequired
-          value={formik.values.email}
-          errors={formik.errors.email}
-          touched={formik.touched.email}
-          onChange={formik.handleChange}
-          onBlur={formik.handleBlur}
-          horizontalConstraint={13}
-        />
-        </Spacings.Stack>
-        <Spacings.Stack scale="s">
-        <TextField
-          name="cancelled"
-          title="cancelled"
-          value={formik.values.employeeNumber}
-          errors={formik.errors.employeeNumber}
-          touched={formik.touched.employeeNumber}
-          onChange={formik.handleChange}
-          onBlur={formik.handleBlur}
-          horizontalConstraint={13}
-        />
-        </Spacings.Stack>
+     
+
+      
      <Spacings.Stack scale="s">
       
         <SelectField
-          name="shipped quantity"
-          title="shipped quantity"
+          name="order workflow status"
+          title="order workflow status"
           value={formik.values.roles}
           errors={formik.errors.roles}
           touched={formik.touched.roles}
@@ -142,21 +92,77 @@ const OrderCreateForm = (props) => {
           horizontalConstraint={13}
         />
         </Spacings.Stack>
-     
+        <Spacings.Stack scale="s">
+      
+      <SelectField
+        name="payment status"
+        title="payment status"
+        value={formik.values.roles}
+        errors={formik.errors.roles}
+        touched={formik.touched.roles}
+        onChange={formik.handleChange}
+        onBlur={formik.handleBlur}
+        
+        options={getTicketPriorityOptions}
+        isReadOnly={props.isReadOnly}
+        isRequired
+        horizontalConstraint={13}
+      />
+      </Spacings.Stack>
+      <Spacings.Stack scale="s">
+      
+        <SelectField
+          name="shipment status"
+          title="shipment status"
+          value={formik.values.roles}
+          errors={formik.errors.roles}
+          touched={formik.touched.roles}
+          onChange={formik.handleChange}
+          onBlur={formik.handleBlur}
+          
+          options={getTicketPriorityOptions}
+          isReadOnly={props.isReadOnly}
+          isRequired
+          horizontalConstraint={13}
+        />
         </Spacings.Stack>
+       
         <Spacings.Stack scale="s">
         <Spacings.Inline>
                 <SecondaryButton
                   onClick={formik.handleReset}
-                  isDisabled={formik.isSubmitting}
-                  label="Reset"
+                  
+                  label="Edit"
                 />
                 <PrimaryButton
                   onClick={formik.handleSubmit}
-                  isDisabled={formik.isSubmitting || !formik.dirty}
+                  
                   label="Submit"
                 />
               </Spacings.Inline>
+              </Spacings.Stack>
+              </Spacings.Stack>
+        </Constraints.Horizontal>
+       
+      {/* </Spacings.Inline> */}
+     </CollapsiblePanel>
+     <CollapsiblePanel
+          data-testid="quote-summary-panel"
+          header={
+            <CollapsiblePanel.Header>
+              {/* {formatMessage(messages.panelTitle)} */}
+              {'Order Items'}
+            </CollapsiblePanel.Header>
+          }
+          scale="l">
+            <Constraints.Horizontal >
+             <Spacings.Stack scale="m">
+            
+             <DataTable rows={rows} columns={columns} />
+     
+
+       
+       
               </Spacings.Stack>
         </Constraints.Horizontal>
        
--- rc-csa/src/components/Orders/components/order-list/order-list.jsx
+++ rc-csa/src/components/Orders/components/order-list/order-list.jsx
@@ -40,10 +40,10 @@ import {
   // SecondaryButton,
   PlusBoldIcon,
 } from '@commercetools-uikit/icons';
-
+import './order-list-module.css';
 import OrderAccount from '../order-account/order-account';
 import { getOrderRows } from 'ct-tickets-helper-api';
-
+import MoneyField from '@commercetools-uikit/money-field';
 // import { getCompanies } from '../../api';
 // import { useEffect } from 'react';
 
@@ -60,20 +60,33 @@ import { getOrderRows } from 'ct-tickets-helper-api';
 
 
 const rows = [
-  { OrderNumber: '00012875',Customer:'Lahari',Created:'jun 14, 2022,2:54:47...',Modified:'Aug 14, 2022,2:54:47...',Status:'In Progress',DeliveryMode:'standard'},
-  { OrderNumber: '00012876',Customer:'women',Created:'Apr 11, 2022,2:54:47...',Modified:'Apr 11, 2022,2:54:47...',Status:'In Progress',DeliveryMode:'standard'},
-  { OrderNumber: '00012877',Customer:'women',Created:'Apr 11, 2022,2:54:47...',Modified:'Apr 11, 2022,2:54:47...',Status:'In Progress',DeliveryMode:'standard'},
-  { OrderNumber: '00012879',Customer:'RanjithKumar',Created:'Nov 11, 2022,2:54:47...',Modified:'Dec 11, 2022,2:54:47...',Status:'In Progress',DeliveryMode:'standard'},
+  { OrderNumber: 'CS0012875',Customer:'Lahari',OrderTotal:'$350.00',NooforderItems:'1',TotalItems:'1',OrderStatus:'',ShipmentStatus:"",PaymentStatus:'',Created:'jun 14, 2022,2:54:47...',Modified:'Aug 14, 2022,2:54:47...'},
+  { OrderNumber: 'CS0012875',Customer:'Lahari',OrderTotal:'$350.00',NooforderItems:'1',TotalItems:'1',OrderStatus:'',ShipmentStatus:"",PaymentStatus:'',Created:'jun 14, 2022,2:54:47...',Modified:'Aug 14, 2022,2:54:47...'},
+  { OrderNumber: 'CS0012875',Customer:'Lahari',OrderTotal:'$350.00',NooforderItems:'1',TotalItems:'1',OrderStatus:'',ShipmentStatus:"",PaymentStatus:'',Created:'jun 14, 2022,2:54:47...',Modified:'Aug 14, 2022,2:54:47...'},
+  // { OrderNumber: '00012876',Customer:'women',Created:'Apr 11, 2022,2:54:47...',Modified:'Apr 11, 2022,2:54:47...',Status:'In Progress',DeliveryMode:'standard'},
+  // { OrderNumber: '00012877',Customer:'women',Created:'Apr 11, 2022,2:54:47...',Modified:'Apr 11, 2022,2:54:47...',Status:'In Progress',DeliveryMode:'standard'},
+  // { OrderNumber: '00012879',Customer:'RanjithKumar',Created:'Nov 11, 2022,2:54:47...',Modified:'Dec 11, 2022,2:54:47...',Status:'In Progress',DeliveryMode:'standard'},
 ];
 
 const columns = [
-
-  { key: 'orderNumber', label: 'Order Number' },
+{ key: 'orderNumber', label: 'Order Number' },
   { key:'customer', label: 'Customer' },
+  { key: 'orderTotal', label: 'Order Total' },
+  { key: 'NooforderItems', label: 'No.of order Items' },
+  { key: 'TotalItems', label: 'Total Items' },
+  { key: 'OrderStatus', label: 'Order Status' },
+  { key: 'ShipmentStatus', label: 'Shipment Status' },
+  { key: 'PaymentStatus', label: 'Payment Status' },
   { key: 'createdAt', label: 'Created' },
   { key: 'lastModifiedAt', label: 'Modified' },
-  { key: 'orderState', label: 'Status' },
-  { key: 'shippingMethodName', label: 'Delivery Mode' },
+  
+
+  // { key: 'orderNumber', label: 'Order Number' },
+  // { key:'customer', label: 'Customer' },
+  // { key: 'createdAt', label: 'Created' },
+  // { key: 'lastModifiedAt', label: 'Modified' },
+  // { key: 'orderState', label: 'Status' },
+  // { key: 'shippingMethodName', label: 'Delivery Mode' },
  
 ];
 
@@ -130,6 +143,22 @@ const Orders =  (props) => {
         size="medium"
       />
       </Spacings.Inline> */}
+      <Spacings.Stack scale="l" className='css-294zjy-container' >
+       
+        <MoneyField
+    title="Price"
+    value={
+    // { amount: '30', currencyCode: 'CustomerEmailAddress' },
+    // { amount: '30', currencyCode: 'FirstName' },
+    // { amount: '30', currencyCode: 'LastName' },
+    { amount: '30', currencyCode: 'OrderNO' },
+    { amount: '30', currencyCode: 'SKU' },
+    { amount: '30', currencyCode: 'Store' }
+  }
+    onChange={(event) => alert(event.target.value)}
+    currencies={['OrderNO','SKU','Store']}
+  />
+  </Spacings.Stack>
       {ordersPaginatedResult?(
         <Spacings.Stack scale="l">
          
