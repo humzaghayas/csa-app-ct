a456caf0c9b76a248b85cc1eb9a0497cac5a6f60
Niroshan R <niroshan.r@royalcyber.com>
2023-03-31 21:05:21 +0530

    Customer cart, order - changes

--- rc-csa/src/components/Customer/components/customer-orders/customer-order.jsx
+++ rc-csa/src/components/Customer/components/customer-orders/customer-order.jsx
@@ -59,16 +59,16 @@ const CustomerOrder = (props) => {
 
   const OrderId = params.id;
 
-  const { projectKey } =useApplicationContext((context) => ({
-    projectKey:context.project.key
+  const { projectKey } = useApplicationContext((context) => ({
+    projectKey: context.project.key
   }));
 
   // const [query] = useState(QUERY);
   const { page, perPage } = usePaginationState();
   const tableSorting = useDataTableSortingState('createdAt desc');
   const customerId = props.customer?.id;
-console.log("params",params);
-console.log("props",props);
+  console.log("params", params);
+  console.log("props", props);
   const { customersOrderPaginatedResult, error, loading } = useCustomersOrdersFetcher({
     page,
     perPage,
@@ -79,57 +79,74 @@ console.log("props",props);
   const columns = [
     { key: 'orderNumber', label: 'Order Number' },
     { key: 'customerEmail', label: 'Email' },
+    { key: 'totalPrice', label: 'Order Total' },
+    { key: 'lineItems', label: 'No of Order Items ' },
+    { key: 'totalItems', label: 'Total Items' },
     { key: 'createdAt', label: 'Date Created' },
     { key: 'shipmentState', label: 'Delivery Mode' },
     { key: 'orderState', label: 'Status' },
   ];
-  
+
   const itemRenderer = (item, column) => {
     switch (column.key) {
       case 'orderNumber':
         return item.orderNumber;
       case 'customerEmail':
         return item.customerEmail;
+      case 'totalPrice':
+        return amountCalculator(
+          item?.totalPrice?.centAmount,
+          item?.totalPrice?.fractionDigits
+        );
+      case 'lineItems':
+        return item?.lineItems?.length;
+      case 'totalItems':
+        return item?.lineItems.map(item => item.quantity).reduce((a, b) => a + b, 0);
       case 'createdAt':
         return item.createdAt;
       case 'shipmentState':
         return "Standard Delivery";
-        case 'orderState':
-         return item.orderState;
+      case 'orderState':
+        return item.orderState;
       default:
         return item[column.key];
     }
   };
 
+  function amountCalculator(centAmount, fractionDigits) {
+    centAmount = centAmount / 100;
+    centAmount = '$' + centAmount + '.00';
+    return centAmount;
+  }
   return (
     <Spacings.Stack scale="xl">
-    
+
       {customersOrderPaginatedResult ? (
         <Spacings.Stack scale="l">
-        <ToggleInput
-    isDisabled={false}
-    isChecked={true}
-     onChange={(event) => alert(event.target.checked)}
-    size="medium"
-  />
+          <ToggleInput
+            isDisabled={false}
+            isChecked={true}
+            onChange={(event) => alert(event.target.checked)}
+            size="medium"
+          />
 
-          
+
           <DataTable
-              isCondensed
-              columns={columns}
-              rows={customersOrderPaginatedResult.results}
-              itemRenderer={(item, column) => itemRenderer(item, column)}
-              maxHeight={600}
+            isCondensed
+            columns={columns}
+            rows={customersOrderPaginatedResult.results}
+            itemRenderer={(item, column) => itemRenderer(item, column)}
+            maxHeight={600}
 
-              onRowClick={(row) => push(`/${projectKey}/${entryPointUriPath}/order-edit/${row.id}/orders-general`)}
-            />
-            <Pagination
-              page={page.value}
-              onPageChange={page.onChange}
-              perPage={perPage.value}
-              onPerPageChange={perPage.onChange}
-              totalItems={customersOrderPaginatedResult.total}
-            />
+            onRowClick={(row) => push(`/${projectKey}/${entryPointUriPath}/order-edit/${row.id}/orders-general`)}
+          />
+          <Pagination
+            page={page.value}
+            onPageChange={page.onChange}
+            perPage={perPage.value}
+            onPerPageChange={perPage.onChange}
+            totalItems={customersOrderPaginatedResult.total}
+          />
 
         </Spacings.Stack>
       ) : "Loading..."}
