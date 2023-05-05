1f08be65d76c7e76563a928cb376725469b3836c
Muhammad Humza Ghayas <humza.ghayas@royalcyber.com>
2023-05-05 13:28:07 +0500

    Terraforms added with CI/CD

--- ct-csa-terraforms/packages/csa-terraform/default/ct-csa-subscriptions.tf
+++ ct-csa-terraforms/packages/csa-terraform/default/ct-csa-subscriptions.tf
@@ -1,15 +1,14 @@
 resource "commercetools_subscription" "csa-order-subscription" {
   key = "csa-order-subscription"
 
-  destination = {
+  destination {
     type          = "GoogleCloudPubSub"
-    projectId     = "${var.gcp_project_id}"
+    project_id     = "${var.gcp_project_id}"
     topic         = "${var.gcp_topic}"
-    region        = "${var.gcp_region}"
    }
 
   message {
     resource_type_id = "order"
-    types            = [ "OrderCreated","OrderStateChanged"]
+    types            = ["OrderCreated","OrderStateChanged"]
   }
 }
