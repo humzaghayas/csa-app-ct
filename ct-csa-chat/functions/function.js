79151a18ffb4a70462b68fce4e44ba30a0b3dc24
KiranVinayP <kiranvinay.p@royalcyber.com>
2023-08-02 22:40:54 +0530

    chat notification

--- ct-csa-chat/functions/function.js
+++ ct-csa-chat/functions/function.js
@@ -44,6 +44,7 @@ async function getChatUpdateIncrement() {
         console.log("Error while creating object in mongodb", error);
       });
 
+    console.log(response);
     if (!response.ok) {
       throw new Error("Network response was not ok");
     }
